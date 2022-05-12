6ab023957f3fae40ef2bb03d9b62094f60a8e237
Xenotester <noob12460@gmail.com>
2022-05-12 12:00:53 +0800

    sad

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,7 +1,7 @@
 const contractAddress = "0x2078d4520E5690d3dcbfF975ac52ba7D5b6bC8a3"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the SCOPX WARRIOR NFT Collection!!";
+const welcome_h1 = "Welcome to the SCOPX WARRIOR NFT Collection!!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
 const welcome_p = 'The SCOPX NFT Project is the first SCOPX NFT Collection. Come get your own SCOPX WARRIOR NFT!!';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
